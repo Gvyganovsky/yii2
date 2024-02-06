1fe8dd8c60fa43b5313486d20a9c710f00b7b98a
gvyganovsky <gvyganovsky@gmail.com>
2024-02-06 15:23:03 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=322-20_up',
+    'username' => '322-20_up',
+    'password' => '666Archer666',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
